bde16b9789108c32c77d36978e8555538f4098a4
Shawn L <shawn.lim@bluescape.com>
2020-01-29 19:20:05 -0800

    more work

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React from "react";
 
 class Home extends React.Component {
-  render(){
+  render() {
     return (
       <div className="home-page-container">
-      <img className="home-page-logo" src='/public/images/solid-saranglimyida.png'/>
+        <div className="home-page-background"></div>
+        <div className="image-container">
+          <div className="image-content">
+            <img
+              className="home-page-logo"
+              src="/public/images/transparent-cropped.png"
+            />
+            <div className="introduction">
+              <p>
+                With great joy, please join us in our celebration officiating
+                our love for each other.
+              </p>
+              <h1 className="names">Shawn Lim</h1>
+              <p>and</p>
+              <h1 className="names">Christine Yi</h1>
+              <p>invites you to save the date for the</p>
+              <p style={{ fontSize: "46px" }}>25th of September, 2020</p>
+              <p style={{ fontSize: "32px" }}>in Uluwatu Bali, Indonesia</p>
+            </div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
